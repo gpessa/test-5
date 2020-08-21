f32fd061c243aaa24930448876238c0b13f73ce3
Gaetano Pessa <g.pessa@goldrepublic.com>
2020-08-21 18:04:06 +0200

    update to latest version of the smart library

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import Upload from './components/Upload';
 import * as mobilenet from "@tensorflow-models/mobilenet";
-import "@tensorflow/tfjs";
+require('@tensorflow/tfjs');
 
 function App() {
   const [model, setModel] = useState<mobilenet.MobileNet>()
   const [file, setFile] = useState<File>()
+  const [name, setName] = useState<string>()
+  
 
   const loadModel = async () => {
     const model = await mobilenet.load()
@@ -17,8 +19,8 @@ function App() {
   }
 
   const onImageLoad = async (event: any) => {
-    const predictions = await model!.classify(event.target)
-    console.log(predictions)
+    const { className } = (await model!.classify(event.target))[0]
+    setName(className)
   };
 
   useEffect(() => {
@@ -31,6 +33,7 @@ function App() {
       {model && 'Loaded'}
       <Upload onChange={setImage} />
       {file && <img src={URL.createObjectURL(file)} onLoad={onImageLoad} />}
+      {name && name}
     </>
   );
 }
