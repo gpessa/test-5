54afea6e50a2a8560b5a8ac39d387be2480224c3
Gaetano Pessa <g.pessa@goldrepublic.com>
2020-08-21 16:56:59 +0200

    - added simple file upload - added deps

--- src/components/Upload/index.tsx
+++ src/components/Upload/index.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState, ChangeEvent } from 'react';
 
-const Upload: React.FC = () => (
-  <h1>dsaas</h1>
-)
+const Upload: React.FC = () => {
+  const [image, setImage] = useState<string>()
+
+  const onFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { files } = event.target
+    if (files) {
+      const url = URL.createObjectURL(files[0])
+      setImage(url)
+    }
+  }
+
+  return (
+    <div>
+      {image && <img src={image} />}
+      <input type="file" onChange={onFileChange} /> 
+    </div>
+  );
+}
 
 export default Upload
